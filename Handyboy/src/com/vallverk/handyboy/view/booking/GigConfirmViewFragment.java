adbb16d27d1eb4fe2f96147f85885f2853ee660f
Артем <timm.kasianov@gmail.com>
2015-02-11 14:44:15 +0200

    Add gig confirm view functional and design

--- Handyboy/src/com/vallverk/handyboy/view/booking/GigConfirmViewFragment.java
+++ Handyboy/src/com/vallverk/handyboy/view/booking/GigConfirmViewFragment.java
@@ -6,18 +6,27 @@ import android.view.View;
 import android.view.View.OnClickListener;
 import android.view.ViewGroup;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 import com.nostra13.universalimageloader.core.DisplayImageOptions;
 import com.nostra13.universalimageloader.core.ImageLoader;
 import com.vallverk.handyboy.R;
+import com.vallverk.handyboy.Tools;
 import com.vallverk.handyboy.model.BookingStatusEnum;
 import com.vallverk.handyboy.model.api.APIManager;
+import com.vallverk.handyboy.model.api.AddonServiceAPIObject;
+import com.vallverk.handyboy.model.api.AddressAPIObject;
 import com.vallverk.handyboy.model.api.BookingAPIObject;
 import com.vallverk.handyboy.model.api.BookingDataManager;
 import com.vallverk.handyboy.model.api.BookingDataObject;
+import com.vallverk.handyboy.model.api.JobAddonsAPIObject;
 import com.vallverk.handyboy.model.api.UserAPIObject;
 import com.vallverk.handyboy.model.api.UserAPIObject.UserParams;
 import com.vallverk.handyboy.view.base.BaseFragment;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 public class GigConfirmViewFragment extends BaseFragment
 {
@@ -33,13 +42,33 @@ public class GigConfirmViewFragment extends BaseFragment
 	private ImageView myAvatarImage;
 	private ImageView handyBoyAvatarImage;
 	private TextView gigTitleTextView;
-	private TextView gigIdTextView;
-	private TextView gigStatusTextView;
+	//private TextView gigIdTextView;
+	private TextView hoursTextView;
+    private TextView gigStatusTextView;
+    private TextView sessionTitle;
+    private  TextView priceTextView;
+    private  TextView addressNameTextView;
+    private TextView addressTextView;
+    private TextView dateTextView;
+    private  TextView timeToTextView;
+    private  TextView timeFromTextView;
+    private  TextView totalPriceTextView;
+
+    private LinearLayout addonContainer;
+    private View detailsTitle;
+
+    private View commentTitle;
+    private TextView commentTextView;
+
+    private View specialReqeustTitle;
+    private TextView specialReqeustTextView;
+
 	
 	private BookingDataManager bookingDataManager;
 	private BookingDataObject bookingDataObject;
 	private DisplayImageOptions avatarLoadOptions = new DisplayImageOptions.Builder ().showImageOnFail ( R.drawable.avatar ).showImageForEmptyUri ( R.drawable.avatar ).cacheInMemory ( true ).cacheOnDisc ().build ();
 
+    private LayoutInflater inflater;
 	public View onCreateView ( LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState )
 	{
 		if ( view == null )
@@ -51,8 +80,29 @@ public class GigConfirmViewFragment extends BaseFragment
 			myAvatarImage = ( ImageView ) view.findViewById ( R.id.myAvatarImage );
 			handyBoyAvatarImage = ( ImageView ) view.findViewById ( R.id.handyBoyAvatarImage );
 			gigTitleTextView = ( TextView ) view.findViewById ( R.id.gigTitleTextView );
-			gigIdTextView = ( TextView ) view.findViewById ( R.id.gigIdTextView );
+			//gigIdTextView = ( TextView ) view.findViewById ( R.id.gigIdTextView );
 			gigStatusTextView = ( TextView ) view.findViewById ( R.id.gigStatusTextView );
+            hoursTextView = (TextView) view.findViewById(R.id.hoursTextView);
+            sessionTitle = (TextView) view.findViewById(R.id.sessionTitle);
+            priceTextView = (TextView) view.findViewById(R.id.priceTextView);
+            addressNameTextView = (TextView) view.findViewById(R.id.addressNameTextView);
+            addressTextView = (TextView) view.findViewById(R.id.addressTextView);
+            dateTextView = (TextView) view.findViewById(R.id.dateTextView);
+            timeToTextView = (TextView) view.findViewById(R.id.timeToTextView);
+            timeFromTextView = (TextView) view.findViewById(R.id.timeFromTextView);
+            totalPriceTextView = (TextView) view.findViewById(R.id.totalPriceTextView);
+
+            addonContainer = (LinearLayout) view.findViewById(R.id.addonContainer);
+            detailsTitle = view.findViewById(R.id.detailsTitle);
+
+            commentTitle = view.findViewById(R.id.commentTitle);
+            commentTextView = (TextView) view.findViewById(R.id.commentTextView);
+
+            specialReqeustTitle = view.findViewById(R.id.specialReqeustTitle);
+            specialReqeustTextView = (TextView) view.findViewById(R.id.specialReqeustTextView);
+
+
+
 		} else
 		{
 			( ( ViewGroup ) view.getParent () ).removeView ( view );
@@ -63,14 +113,7 @@ public class GigConfirmViewFragment extends BaseFragment
 	@Override
 	protected void init ()
 	{
-		addListeners ();
-		ImageLoader.getInstance ().displayImage ( user.getString ( UserParams.AVATAR ), myAvatarImage, avatarLoadOptions );
-		ImageLoader.getInstance ().displayImage (service.getString ( UserParams.AVATAR ), handyBoyAvatarImage, avatarLoadOptions );
-		gigTitleTextView.setText ( "YOU + " + service.getShortName ().toUpperCase () );
-		gigIdTextView.setText ( "GIG#" + bookingDataObject.getId ().toString () );
-		BookingStatusEnum status = bookingDataObject.getSatus ();
-		gigStatusTextView.setText ( status.name () );
-		gigStatusTextView.setBackgroundColor ( BookingAPIObject.getStatusColor ( status ) );
+
 	}
 
 	@Override
@@ -84,6 +127,87 @@ public class GigConfirmViewFragment extends BaseFragment
 		service = bookingDataObject.getService ();
 		userBooking =  bookingDataObject.getCustomer();
 		isIService =  service.getId ().equals ( apiManager.getUser ().getId () );
+
+        inflater = LayoutInflater.from(controller);
+
+
+        ImageLoader.getInstance ().displayImage ( user.getString ( UserParams.AVATAR ), myAvatarImage, avatarLoadOptions );
+        ImageLoader.getInstance ().displayImage (service.getString ( UserParams.AVATAR ), handyBoyAvatarImage, avatarLoadOptions );
+        gigTitleTextView.setText ( "YOU + " + service.getShortName ().toUpperCase () );
+       // gigIdTextView.setText ( "GIG#" + bookingDataObject.getId ().toString () );
+        BookingStatusEnum status = bookingDataObject.getSatus ();
+        gigStatusTextView.setText ( status.name () );
+        gigStatusTextView.setBackgroundColor ( BookingAPIObject.getStatusColor ( status ) );
+
+        hoursTextView.setText(bookingDataObject.getBookingAPIObject ().getValue ( BookingAPIObject.BookingAPIParams.TOTAL_HOURS ).toString ());
+        sessionTitle.setText( bookingDataObject.getTypeJobAPIObject ().getName () + " Session");
+        priceTextView.setText ( "$" + bookingDataObject.getBookingAPIObject ().getValue ( BookingAPIObject.BookingAPIParams.TOTAL_PRICE ).toString () );
+        addressNameTextView.setText ( bookingDataObject.getAddress ().getString ( AddressAPIObject.AddressParams.DESCRIPTION ) );
+        addressTextView.setText ( bookingDataObject.getAddress ().getString ( AddressAPIObject.AddressParams.ADDRESS ) );
+
+        dateTextView.setText ( Tools.toDateString (bookingDataObject.getBookingAPIObject().getString(BookingAPIObject.BookingAPIParams.DATE)) );
+
+        JSONArray timeJsonArray;
+        JSONObject timePeriod;
+        String startPeriod = "";
+        String endPeriod = "";
+        try
+        {
+            timeJsonArray = new JSONArray( bookingDataObject.getBookingAPIObject ().getValue ( BookingAPIObject.BookingAPIParams.TIME ).toString () );
+            timePeriod = timeJsonArray.getJSONObject ( 0 );
+            startPeriod = timePeriod.getString ( "start" );
+            endPeriod = timePeriod.getString ( "end" );
+        } catch ( JSONException e )
+        {
+            e.printStackTrace ();
+        }
+        timeToTextView.setText(endPeriod);
+        timeFromTextView.setText(startPeriod);
+        totalPriceTextView.setText( "$" + bookingDataObject.getBookingAPIObject ().getValue ( BookingAPIObject.BookingAPIParams.TOTAL_PRICE ).toString ());
+
+        addonContainer.removeAllViews ();
+
+        if(bookingDataObject.getAddonsAPIObjects ().size() > 0){
+            detailsTitle.setVisibility(View.VISIBLE);
+        }else{
+            detailsTitle.setVisibility(View.GONE);
+        }
+
+        for ( BookingDataObject.JobAddonDetailsObject jobAddonDetailsObject : bookingDataObject.getAddonsAPIObjects () )
+        {
+            View addonItemView = inflater.inflate ( R.layout.addon_confirm_item_view, null );
+            TextView addonNameTextView = ( TextView ) addonItemView.findViewById ( R.id.addonNameTextView );
+            TextView addonPriceTextView = ( TextView ) addonItemView.findViewById ( R.id.addonPriceTextView );
+            addonNameTextView.setText ( jobAddonDetailsObject.addonsAPIObject.getValue ( JobAddonsAPIObject.JobAddonsAPIParams.NAME ).toString() );
+            addonPriceTextView.setText ( "$" + jobAddonDetailsObject.addonServiceAPIObject.getValue ( AddonServiceAPIObject.AddonServiceAPIParams.PRICE ) );
+            addonContainer.addView ( addonItemView );
+        }
+
+        String comment = bookingDataObject.getBookingAPIObject ().getValue ( BookingAPIObject.BookingAPIParams.COMMENT ).toString ();
+        if ( comment.isEmpty () )
+        {
+            commentTitle.setVisibility(View.GONE);
+            commentTextView.setVisibility ( View.GONE );
+        } else
+        {
+            commentTextView.setText ( comment );
+            commentTitle.setVisibility(View.VISIBLE);
+            commentTextView.setVisibility ( View.VISIBLE );
+        }
+
+        String specialRequest = bookingDataObject.getBookingAPIObject ().getValue ( BookingAPIObject.BookingAPIParams.SPECIAL_REQUEST ).toString ();
+        if ( specialRequest.isEmpty () )
+        {
+            specialReqeustTitle.setVisibility(View.GONE);
+            specialReqeustTextView.setVisibility ( View.GONE );
+        } else
+        {
+            specialReqeustTextView.setText ( specialRequest );
+            specialReqeustTitle.setVisibility(View.VISIBLE);
+            specialReqeustTextView.setVisibility ( View.VISIBLE );
+        }
+
+        addListeners();
 	}
 	
 	private void addListeners ()
